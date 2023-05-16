71577a1360e4ca85b7fe4334d7122c0f2bee13d7
bast38900 <bast38900@edu.ucl.dk>
2023-05-16 16:57:15 +0200

    splashAndroid

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {SigninScreen} from './app/screens';
+import SplashScreen from 'react-native-splash-screen';
+import {useEffect} from 'react';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
