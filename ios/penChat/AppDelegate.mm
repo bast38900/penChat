c71875db2e801e3f78fd1982acbefcd5c0efcbc0
bast38900 <bast38900@edu.ucl.dk>
2023-05-19 12:46:46 +0200

    firebaseIOS

--- ios/penChat/AppDelegate.mm
+++ ios/penChat/AppDelegate.mm
@@ -2,11 +2,13 @@
 #import "RNSplashScreen.h"
 #import <React/RCTRootView.h>
 #import <React/RCTBundleURLProvider.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   self.moduleName = @"penChat";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
