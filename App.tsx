ffc2d9523028cff637ad6dfb933f268321a64635
bast38900 <bast38900@edu.ucl.dk>
2023-05-25 10:49:56 +0200

    ContextAndProvider...

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
+import {AuthProvider} from './app/context/auth.context';
 import {RootNavigator} from './app/navigation/root.navigator';
 import {PaperProvider} from 'react-native-paper';
 
 export default function App() {
   return (
     <PaperProvider>
-      <RootNavigator />
+      <AuthProvider>
+        <RootNavigator />
+      </AuthProvider>
     </PaperProvider>
   );
 }
--- app/context/auth.context.tsx
+++ app/context/auth.context.tsx
@@ -1,7 +1,10 @@
 import React, {createContext, useState} from 'react';
 import auth from '@react-native-firebase/auth';
 
-export const AuthContext = createContext({});
+export const AuthContext = createContext({
+  user: null,
+  setUser: (user: any) => {},
+});
 
 export const AuthProvider = ({children}: React.PropsWithChildren<unknown>) => {
   const [user, setUser] = useState(null);
